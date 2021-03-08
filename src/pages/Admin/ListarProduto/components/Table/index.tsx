e4021a5c6af82c0cf16283a946884f0ce8276661
Danilo <daniloranea@gmail.com>
2021-03-08 11:28:56 -0300

    criando listagem de produtos

--- src/pages/Admin/ListarProduto/components/Table/index.tsx
+++ src/pages/Admin/ListarProduto/components/Table/index.tsx
@@ -1,74 +1,121 @@
 import react from 'react';
 import './styles.scss';
+import DataTable from 'react-data-table-component';
 
-// type DataTable = {
-//     titulo: string;
-//     Editora: string;
-//     Autor: string;
-//     categoria: string;
-//     QtdEtq: string;
-//     Status: string;
-//     Preço: string;
-//     Editar: string;
-//     InativarReativar: string;
-//     Visualizar: string;
-// }
+export const listagemProduto = [
+    {titulo: 'Aaaaaaa', Editora: 'bbbbbbb', Autor: 'ccccccc', categoria: 'ddddddd', QtdEtq: 'eeeeeee',
+            Status: 'fffffff', Preço: 'ggggggg', 
+            Editar: 'hhhhhhh', InativarReativar: 'iiiiiii', Visualizar:'jjjjjjj'},
+            {titulo: 'Aaaaaaa', Editora: 'bbbbbbb', Autor: 'ccccccc', categoria: 'ddddddd', QtdEtq: 'eeeeeee',
+            Status: 'fffffff', Preço: 'ggggggg', 
+            Editar: 'hhhhhhh', InativarReativar: 'iiiiiii', Visualizar:'jjjjjjj'},
+            {titulo: 'Aaaaaaa', Editora: 'bbbbbbb', Autor: 'ccccccc', categoria: 'ddddddd', QtdEtq: 'eeeeeee',
+            Status: 'fffffff', Preço: 'ggggggg', 
+            Editar: 'hhhhhhh', InativarReativar: 'iiiiiii', Visualizar:'jjjjjjj'},
+            {titulo: 'Aaaaaaa', Editora: 'bbbbbbb', Autor: 'ccccccc', categoria: 'ddddddd', QtdEtq: 'eeeeeee',
+            Status: 'fffffff', Preço: 'ggggggg', 
+            Editar: 'hhhhhhh', InativarReativar: 'iiiiiii', Visualizar:'jjjjjjj'},
+            {titulo: 'Aaaaaaa', Editora: 'bbbbbbb', Autor: 'ccccccc', categoria: 'ddddddd', QtdEtq: 'eeeeeee',
+            Status: 'fffffff', Preço: 'ggggggg', 
+            Editar: 'hhhhhhh', InativarReativar: 'iiiiiii', Visualizar:'jjjjjjj'},
+            {titulo: 'Aaaaaaa', Editora: 'bbbbbbb', Autor: 'ccccccc', categoria: 'ddddddd', QtdEtq: 'eeeeeee',
+            Status: 'fffffff', Preço: 'ggggggg', 
+            Editar: 'hhhhhhh', InativarReativar: 'iiiiiii', Visualizar:'jjjjjjj'},
+            {titulo: 'Aaaaaaa', Editora: 'bbbbbbb', Autor: 'ccccccc', categoria: 'ddddddd', QtdEtq: 'eeeeeee',
+            Status: 'fffffff', Preço: 'ggggggg', 
+            Editar: 'hhhhhhh', InativarReativar: 'iiiiiii', Visualizar:'jjjjjjj'},
+            {titulo: 'Aaaaaaa', Editora: 'bbbbbbb', Autor: 'ccccccc', categoria: 'ddddddd', QtdEtq: 'eeeeeee',
+            Status: 'fffffff', Preço: 'ggggggg', 
+            Editar: 'hhhhhhh', InativarReativar: 'iiiiiii', Visualizar:'jjjjjjj'},
+            {titulo: 'Aaaaaaa', Editora: 'bbbbbbb', Autor: 'ccccccc', categoria: 'ddddddd', QtdEtq: 'eeeeeee',
+            Status: 'fffffff', Preço: 'ggggggg', 
+            Editar: 'hhhhhhh', InativarReativar: 'iiiiiii', Visualizar:'jjjjjjj'},
+            {titulo: 'Aaaaaaa', Editora: 'bbbbbbb', Autor: 'ccccccc', categoria: 'ddddddd', QtdEtq: 'eeeeeee',
+            Status: 'fffffff', Preço: 'ggggggg', 
+            Editar: 'hhhhhhh', InativarReativar: 'iiiiiii', Visualizar:'jjjjjjj'},
+            {titulo: 'Aaaaaaa', Editora: 'bbbbbbb', Autor: 'ccccccc', categoria: 'ddddddd', QtdEtq: 'eeeeeee',
+            Status: 'fffffff', Preço: 'ggggggg', 
+            Editar: 'hhhhhhh', InativarReativar: 'iiiiiii', Visualizar:'jjjjjjj'},
+            {titulo: 'Aaaaaaa', Editora: 'bbbbbbb', Autor: 'ccccccc', categoria: 'ddddddd', QtdEtq: 'eeeeeee',
+            Status: 'fffffff', Preço: 'ggggggg', 
+            Editar: 'hhhhhhh', InativarReativar: 'iiiiiii', Visualizar:'jjjjjjj'},
+            {titulo: 'Aaaaaaa', Editora: 'bbbbbbb', Autor: 'ccccccc', categoria: 'ddddddd', QtdEtq: 'eeeeeee',
+            Status: 'fffffff', Preço: 'ggggggg', 
+            Editar: 'hhhhhhh', InativarReativar: 'iiiiiii', Visualizar:'jjjjjjj'},
+            {titulo: 'Aaaaaaa', Editora: 'bbbbbbb', Autor: 'ccccccc', categoria: 'ddddddd', QtdEtq: 'eeeeeee',
+            Status: 'fffffff', Preço: 'ggggggg', 
+            Editar: 'hhhhhhh', InativarReativar: 'iiiiiii', Visualizar:'jjjjjjj'},
+            {titulo: 'Aaaaaaa', Editora: 'bbbbbbb', Autor: 'ccccccc', categoria: 'ddddddd', QtdEtq: 'eeeeeee',
+            Status: 'fffffff', Preço: 'ggggggg', 
+            Editar: 'hhhhhhh', InativarReativar: 'iiiiiii', Visualizar:'jjjjjjj'},
+            {titulo: 'Aaaaaaa', Editora: 'bbbbbbb', Autor: 'ccccccc', categoria: 'ddddddd', QtdEtq: 'eeeeeee',
+            Status: 'fffffff', Preço: 'ggggggg', 
+            Editar: 'hhhhhhh', InativarReativar: 'iiiiiii', Visualizar:'jjjjjjj'},
+            {titulo: 'Aaaaaaa', Editora: 'bbbbbbb', Autor: 'ccccccc', categoria: 'ddddddd', QtdEtq: 'eeeeeee',
+            Status: 'fffffff', Preço: 'ggggggg', 
+            Editar: 'hhhhhhh', InativarReativar: 'iiiiiii', Visualizar:'jjjjjjj'},
+            {titulo: 'Aaaaaaa', Editora: 'bbbbbbb', Autor: 'ccccccc', categoria: 'ddddddd', QtdEtq: 'eeeeeee',
+            Status: 'fffffff', Preço: 'ggggggg', 
+            Editar: 'hhhhhhh', InativarReativar: 'iiiiiii', Visualizar:'jjjjjjj'},
 
-// type Props = {
-//     data: DataTable[]
-// }
+]
 
-// const Table = ({ data }: Props) => {
+export const colunas = [
+    {
+        name: "titulo",
+        selector: "titulo",
+        sortable:true
+    },
+    {
+        name: "Editora",
+        selector: "Editora",
+        sortable:true
+    },
+    {
+        name: "Autor",
+        selector: "Autor",
+        sortable:true
+    },
+    {
+        name: "categoria",
+        selector: "categoria",
+        sortable:true
+    },
+    {
+        name: "QtdEtq",
+        selector: "QtdEtq",
+        sortable:true
+    },
+    {
+        name: "Status",
+        selector: "Status",
+        sortable:true
+    },
+    {
+        name: "Preço",
+        selector: "Preço",
+        sortable:true
+    },
+    {
+        name: "Editar",
+        selector: "Editar",
+        sortable:true
+    },
+    {
+        name: "InativarReativar",
+        selector: "InativarReativar",
+        sortable:true
+    },
+    {
+        name: "Visualizar",
+        selector: "Visualizar",
+        sortable:true
+    },
+]
 
-//     const keys = Object.keys(data[0]);
-//     const Row = ({ anchor }: AlignSetting) => {
-
-//         const keys = Object.keys(anchor);
-//         return (
-//             <tr key={anchor.arguments}>
-//                 {
-//                     keys.map(key => <td key={key}></td>)
-//                 }
-
-//             </tr>
-//         )
-//     }
-//     return (
-//         <table className="table">
-//             <thead>
-//                 <tr>
-//                     {
-//                         keys.map(key => <th key={key}>{key}</th>)
-//                     }
-//                 </tr>
-//             </thead>
-//             <tbody>
-//                 {
-//                     data.map(record => <Row record={record} />)
-//                 }
-
-//             </tbody>
-//         </table>
-
-//     )
-// }
-
-const Table = () => {
-    return (
-        <table className="table">
-            <thead>
-                <tr>
-                    Título
-                </tr>
-            </thead>
-            <tbody>
-                <td>
-                    Linhas
-                </td>
-            </tbody>
-        </table>
-
-    )
+export const paginationOptions = {
+    rowsPerPageText: "Livros por Página",
+    rangeSeparatorText: 'de',
+    selectAllRowsItem: true,
+    selectAllRowsItemText: 'Todos'
 }
-
-export default Table
