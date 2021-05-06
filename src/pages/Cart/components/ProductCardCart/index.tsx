22112bc15c5fc91963991e4c761ff18d159203df
Paulo Honorato <paulohonoratos@hotmail.com>
2021-05-06 00:36:50 -0300

    adicionando funcionalidade ao carrinho de compras

---
+++ src/pages/Cart/components/ProductCardCart/index.tsx
@@ -0,0 +1,108 @@
+import { constants } from "node:os";
+import { useEffect, useState } from "react"
+import { components } from "react-select";
+import lixeira from '../../../../../src/core/assets/images/lixeira.svg'
+import { Product, ProductsResponse } from "../../../../core/components/types/Product";
+import { makeRequest } from "../../../../services/api";
+import './styles.scss'
+
+type Props = {
+    product: Product;
+}
+
+const ProductCardCart = ({ product }: Props) => {
+    const [counter, setCounter] = useState(1);
+    const [productPrice, setProductPrice] = useState(product.price);
+    const [activePage, setActivePage] = useState(0);
+    const [products, setProducts] = useState<Product[]>([]);
+
+    const onAdd = () => {
+        if (counter < product.quantity) {
+            setCounter(counter + 1);
+            setProductPrice(productPrice + product.price);
+        }
+    }
+
+    const onDecrement = () => {
+        if (counter > 1) {
+            setCounter(counter - 1);
+            setProductPrice(productPrice - product.price);
+        }
+    }
+    
+    const removeProduct = (productId: number) => {
+        console.log('remover')
+        const cartData = localStorage.getItem("cartData") ?? '{}';
+  		const parsedCartData: Product[] = JSON.parse(cartData);
+        parsedCartData.forEach((element, i) => {
+            if(element.id === productId) {
+                parsedCartData.splice(i, 1)
+            }
+        });
+        if(parsedCartData.length === 0) {
+            localStorage.clear()
+        } else {
+            setProducts(parsedCartData)
+            localStorage.setItem('cartData', JSON.stringify(parsedCartData))
+        }
+
+        window.location.reload()
+    }
+
+    return (
+        <div className="card card-geral">
+            <div className="card-body">
+                <div className="geral row">
+                    <div className="card card-image">
+                        {product.images.map(image => (
+                            image.principal && (
+                                <img src={image.imgUrl} alt={image.imgUrl} />
+                            )
+                        ))}
+                    </div>
+
+                    <div className="col-8 teste">
+                        <div className="titulos">
+                            <h6 className="titulo-produto"><strong>produto</strong></h6>
+                            <h6 className="titulo-qtd"><strong>qtd.</strong></h6>
+                            <h6 className="titulo-preco"><strong>preço</strong></h6>
+                        </div>
+
+                        <div className="row infos mt-3">
+                            <label className="nomeLivro col-4">
+                                {product.title}
+                            </label>
+
+                            <div className="quantidade col-1">
+                                <button
+                                    className="decrementar btn btn-primary"
+                                    onClick={onDecrement}
+                                >
+                                    -
+                                    </button>
+                                <h5 className="qtd">{counter}</h5>
+                                <button
+                                    className="incrementar btn btn-primary"
+                                    onClick={onAdd}
+                                >
+                                    +
+                                    </button>
+                            </div>
+
+                            <div className="preco col-4">
+                                <label><strong>R$ {productPrice}</strong></label>
+                                <button
+                                    className="remove btn btn-danger"
+                                    onClick={() => removeProduct(product.id)}>
+                                    <img src={lixeira} alt="remover" />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductCardCart;
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,19 +1,47 @@
-import ProductCard from './components/ProductCard'
+import ProductCardCart from './components/ProductCardCart'
 import OrderSummary from './components/OrderSummary'
+import { Product } from '../../core/components/types/Product'
+
+import './styles.scss'
+import { Link } from 'react-router-dom'
 
 const Cart = () => {
+
+    const getCartData = () => {
+        const cartData = localStorage.getItem("cartData") ?? '{}';
+        const parsedCartData = JSON.parse(cartData);
+        return parsedCartData as Product[];
+    }
+
+    if (!localStorage.getItem("cartData")) {
+        return (
+            <div className="cesta-vazia">
+                <h5>SUA CESTA ESTÁ VAZIA</h5>
+                <br />
+                <button className="btn btn-primary">
+                    <Link to="/">
+                        Começar a comprar
+					</Link>
+                </button>
+            </div>
+
+        )
+    }
     return (
         <>
             <div className="row">
-                <div className="col-9 mt-2">
-                    <ProductCard />
-                </div>
+                {getCartData()?.map(book => (
+                    <div className="col-9 mt-2">
+                        <ProductCardCart product={book} />
+                    </div>
+                ))}
                 <div className="col-3 mt-2">
                     <OrderSummary />
                 </div>
             </div>
         </>
     )
+
 }
 
 export default Cart;
