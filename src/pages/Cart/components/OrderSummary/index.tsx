d8c9d4b6523663579bb4fcccbdecac5a02d926ca
Paulo Honorato <paulohonoratos@hotmail.com>
2021-05-06 22:46:27 -0300

    consumo de api para calculo de frete

--- src/pages/Cart/components/OrderSummary/index.tsx
+++ src/pages/Cart/components/OrderSummary/index.tsx
@@ -1,6 +1,48 @@
+import axios from 'axios'
+import { useEffect } from 'react'
+import { makeRequest } from '../../../../services/api'
+
+import { calcularPrecoPrazo } from 'correios-brasil';
 import './styles.scss'
 
 const OrderSummary = () => {
+    // useEffect(() => {
+    //     let args = {
+    //     // Não se preocupe com a formatação dos valores de entrada do cep, qualquer uma será válida (ex: 21770-200, 21770 200, 21asa!770@###200 e etc),
+    //     sCepOrigem: '05588001',
+    //     sCepDestino: '30380472',
+    //     nVlPeso: '1',
+    //     nCdFormato: '1',
+    //     nVlComprimento: '20',
+    //     nVlAltura: '20',
+    //     nVlLargura: '20',
+    //     nCdServico: ['04014', '04510'], //Array com os códigos de serviço
+    //     nVlDiametro: '0',
+    //     };
+
+    //     console.log('fazendo requisição')
+    //     calcularPrecoPrazo(args).then((response) => {
+    //         console.log(response)
+    //     })
+    // }, [])
+
+    const url = () => {
+        console.log('fazendo requisição')
+        axios.defaults.baseURL = 'https://ws.correios.com.br';
+        axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
+        axios.get("/calculador/CalcPrecoPrazo.aspx?nCdServico=04014&sCepOrigem=04848010&sCepDestino=31230340&nVlPeso=1&nCdFormato=1&nVlComprimento=30&nVlAltura=10&nVlLargura=20&nVlDiametro=35&sCdMaoPropria=n&nVlValorDeclarado=0&sCdAvisoRecebimento=n&StrRetorno=xml")
+            .then(response => {
+                console.log(response)
+            })
+            .catch(response => {
+                console.log(response)
+            })
+    }
+
+    useEffect(() => {
+        url()
+    }, [])
+
     return (
         <div className="card container">
             <h5><strong>resumo do pedido</strong></h5>
