65852eb384063a8672bdd1318a1480732cfe0095
Paulo Honorato <paulohonoratos@hotmail.com>
2021-05-11 22:22:04 -0300

    adicionando listagem de endereços para escolher entrega

--- src/pages/Cart/components/Checkout/index.tsx
+++ src/pages/Cart/components/Checkout/index.tsx
@@ -4,21 +4,32 @@ import { Address, Client } from '../../../../core/components/types/Client'
 import { Product } from '../../../../core/components/types/Product'
 import { getCartData, ProductsCart } from '../../../../core/components/utils/cart'
 import { makePrivateRequest } from '../../../../services/api'
+import OrderSummary from '../OrderSummary'
+import Card from '../Checkout/components/Card'
 
 import './styles.scss'
+import { useParams } from 'react-router'
 
-const Checkout = () => {
+type ParamsType = {
+  clientId: string;
+}
+
+const Checkout: React.FC = () => {
   const [customer, setCustomer] = useState<Client>()
   const [products, setProducts] = useState<ProductsCart[]>();
   const [quantidadeTotalDeProdutos, setQuantidadeTotalDeProdutos] = useState(0);
   const [valorTotalDeLivros, setValorTotalDeLivros] = useState(0);
   const [valorTotal, setValorTotal] = useState(0);
   const [shipping, setShipping] = useState(0);
+  const [updateSummaryCart, setUpdateSummaryCart] = useState(false);
+  const getCart = getCartData();
+  const [addresses, setAddresses] = useState<Address[]>();
+  const { clientId } = useParams<ParamsType>();
 
   useEffect(() => {
     const data = getCartData()
     setProducts(data.products)
-    
+
     let sum = 0
     products?.map(book => {
       sum += book.product?.price * book.sellQuantity
@@ -32,24 +43,47 @@ const Checkout = () => {
     setQuantidadeTotalDeProdutos(sumQuantidadeTotalDeProdutos)
 
     setValorTotal(sum + 0)
-    
+
     makePrivateRequest({ url: `/clients/${data.customerId}` })
       .then(response => {
         console.log(response)
         setCustomer(response.data)
+        setAddresses(response.data.addresses)
       })
   }, [])
+
+  const onDisabled = (addressId: number) => {
+
+  }
+
+  const onPaymentChange = (addressPayment: boolean, addressIdPayment: number) => {
+
+  }
 
   return (
     <div className="resume">
       <div className="resume-customer">
-        endereço de entrega
-        <span>{customer?.fistName}</span>
+        {addresses?.map(address => (
+          <Card
+            address={address}
+            clientId={clientId}
+            onDisabled={onDisabled}
+            buttonTitle={'INATIVAR'}
+            key={address.id}
+            onPaymentChange={onPaymentChange}
+          />
+        ))}
+        {/* <span>{customer?.fistName}</span> */}
       </div>
       <div className="resume-products">
-        <h5><strong>resumo do pedido</strong></h5>
-        <div className="card-body row">
-          {quantidadeTotalDeProdutos > 1 ? (
+        <div className="summary">
+          <OrderSummary
+            books={getCart.products}
+            updateSummaryCart={updateSummaryCart}
+            key={getCart.customerId}
+          />
+        </div>
+        {/* {quantidadeTotalDeProdutos > 1 ? (
             <h6 className="col-6">{quantidadeTotalDeProdutos} produtos</h6>
           ) : (
             <h6 className="col-6">{quantidadeTotalDeProdutos} produto</h6>
@@ -60,8 +94,7 @@ const Checkout = () => {
 
           <div className="linha col-12"></div>
           <h5 className="col-6"><strong>total</strong></h5>
-          <h5 className="col-6 valor"><strong>R$ {valorTotal.toFixed(2).replace(".", ",")}</strong></h5>
-        </div>
+          <h5 className="col-6 valor"><strong>R$ {valorTotal.toFixed(2).replace(".", ",")}</strong></h5> */}
       </div>
     </div>
   )
